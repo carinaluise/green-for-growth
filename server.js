b859f6a9f3db3b9323c4daf3cd08d35c3a413bd7
Carina <carinalrobinson@gmail.com>
2020-11-29 19:40:44 +0100

    change of design and impleneted contact form with backend

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 
 const express = require('express')
-const app = express();
 const path = require('path');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const { body,validationResult } = require('express-validator');
+const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser');
+const app = express();
+
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+
+require('dotenv').config()
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({
+  extended: true
+}))
+
 
 app.route('/recipes')
  .get((req, res) => {
@@ -21,9 +37,66 @@ app.route('/contact')
   res.sendFile(path.join(__dirname + '/pages/contact.html'));
 });
 
+app.route('/submit-form')
+  .post([body('email').isEmail()],(req,res) => {
+    const errors = validationResult(req);
+    const email = req.body.email
+    const topic = req.body.topic
+    const content = req.body.content
+  
+      const myOAuth2Client = new OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+      )
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`)
+      myOAuth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN
+        });
+
+
+      const myAccessToken = myOAuth2Client.getAccessToken()
+
+      const transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+             type: "OAuth2",
+             user: process.env.SECRET_EMAIL, //your gmail account you used to set the project up in google cloud console"
+             clientId: process.env.CLIENT_ID,
+             clientSecret: process.env.CLIENT_SECRET,
+             refreshToken: process.env.REFRESH_TOKEN,
+             accessToken: myAccessToken //access token variable we defined earlier
+        }});
+
+    const emailOptions = {
+      from: email, 
+      to: process.env.SECRET_EMAIL, 
+      subject: topic, 
+      text: content
+  };
+
+    if (!errors.isEmpty()) {
+      return res.redirect('/contact?error=' + encodeURIComponent('Incorrect_Credential'));  
+      // return res.status(400).json({errors: errors.array()});
+
+    } else {
+      transport.sendMail(emailOptions, function(error, info){
+        if(error){
+            console.log('Message Error - please check your credentials and try again')
+            res.redirect("/contact")
+        }else{
+            console.log('Message sent: ' + info.response);
+            alert("Your email has been successfully sent")
+            res.redirect("/");
+        };
+    });
+    }
+  
+  });
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`)
 });
 
 
