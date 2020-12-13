38bd610a8d93e3dab94841ba57891a365a891668
Carina <carinalrobinson@gmail.com>
2020-12-13 15:49:35 +0100

    added src folder

---
+++ src/search.js
@@ -0,0 +1,13 @@
+
+
+
+// const searchBar = document.getElementById('searchBar');
+
+// searchBar.addEventListener('keyup' , (e) => {
+
+// const searchString = e.target.value;
+// console.log(searchString);
+
+
+
+// })
