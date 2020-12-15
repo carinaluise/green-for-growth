0621f27a2f2aa1bf55024554999d7076a4e94a01
Carina <carinalrobinson@gmail.com>
2020-12-15 22:38:25 +0100

    Changed home route took away/public as test

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({
 
 app.route('/')
 .get((req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 
